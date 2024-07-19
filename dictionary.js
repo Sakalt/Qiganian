cc72774e7cb4c8fd4b1dae91a1bcdfbfb138567e
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-19 17:37:34 +0900

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,93 @@
+const dictionary = [
+    {
+        word: "yu",
+        pronunciation: "ü",
+        partOfSpeech: "名詞",
+        meaning: "お湯",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "mushi",
+        pronunciation: "mushi",
+        partOfSpeech: "名詞",
+        meaning: "虫",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "ashi",
+        pronunciation: "æshi",
+        partOfSpeech: "名詞",
+        meaning: "足",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "te",
+        pronunciation: "tē",
+        partOfSpeech: "名詞",
+        meaning: "手",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "otoge",
+        pronunciation: "otoge",
+        partOfSpeech: "名詞",
+        meaning: "頤",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "hitai",
+        pronunciation: "hitai",
+        partOfSpeech: "名詞",
+        meaning: "額",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "ago",
+        pronunciation: "aɡo",
+        partOfSpeech: "名詞",
+        meaning: "顎",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "kao",
+        pronunciation: "kao",
+        partOfSpeech: "名詞",
+        meaning: "顔",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "watashi",
+        pronunciation: "watashi",
+        partOfSpeech: "名詞",
+        meaning: "私",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "ringo",
+        pronunciation: "riŋgo",
+        partOfSpeech: "名詞",
+        meaning: "リンゴ",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "taberu",
+        pronunciation: "tabeɾu",
+        partOfSpeech: "動詞",
+        meaning: "食べる",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "wa",
+        pronunciation: "wa",
+        partOfSpeech: "助詞",
+        meaning: "主語を示す助詞",
+        example: "watashi wa ringo wo taberu"
+    },
+    {
+        word: "wo",
+        pronunciation: "wo",
+        partOfSpeech: "助詞",
+        meaning: "目的語を示す助詞",
+        example: "watashi wa ringo wo taberu"
+    }
+];
